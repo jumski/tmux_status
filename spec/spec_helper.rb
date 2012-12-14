686a77b474782eee3dd57cf0bbba6362dcd9030c
Wojtek Majewski <jumski@gmail.com>
2012-12-14 11:19:54 +0100

    spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+current_dir = File.dirname(__FILE__)
+
+require 'rspec'
+require 'mocha'
+require 'pry'
+require File.join(current_dir, '../lib/tmux_status')
+
+Dir[File.join(current_dir, 'spec/support/**/*.rb')].each do |path|
+  require path
+end
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
