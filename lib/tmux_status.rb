a7d0afac71e4f6a10380de84f18f0444f12c1343
Wojtek Majewski <jumski@gmail.com>
2012-12-14 20:59:00 +0100

    fix naming

--- lib/tmux_status.rb
+++ lib/tmux_status.rb
@@ -6,5 +6,5 @@ require "tmux_status/version"
 require "tmux_status/unimplemented_error"
 require "tmux_status/segment"
 require "tmux_status/segments/concatenator"
-require "tmux_status/segments/mocp_player"
+require "tmux_status/segments/mocp_status"
 
