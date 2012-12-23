7408a4e417c60abe38ffbcbf5924c1926fdf6004
Wojtek Majewski <jumski@gmail.com>
2012-12-23 01:40:34 +0100

    date time separator

--- lib/tmux_status/segments/date_time.rb
+++ lib/tmux_status/segments/date_time.rb
@@ -5,7 +5,7 @@ module TmuxStatus
   module Segments
     class DateTime < Segment
       DEFAULT_OPTIONS = {
-        bg: 16, fg: 237, bold: false, format: '%H:%M %d-%m-%Y'
+        bg: 16, fg: 247, bold: false, format: '%H:%M ⮃ %d-%m-%Y'
       }
 
       def initialize(options = {})
